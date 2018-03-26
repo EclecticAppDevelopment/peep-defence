f7e6c75ecbe3b8418cd0794295bdc9c535c6b51d
Matt <eclecticappdevelopment@gmail.com>
2018-03-26 16:45:56 +0100

    Update tower-defence.php

--- tower-defence.php
+++ tower-defence.php
@@ -122,30 +122,52 @@ include $partials_folder . '_navbar.php';
 		
 		<div class='w3-row'>
 		
-			<div class='w3-col s12 m6'>
+			<div id='canvasWrapper' class='w3-col s12 m6'>
 				<canvas id='canvas' width='100%' height='100%'>
-					Sorry, your browser does not support the canvas element
+					Sorry, your browser does not support the canvas element (Foreground)
+				</canvas>
+				<canvas id='bgCanvas' width='100%' height='100%'>
+					Sorry, your browser does not support the canvas element (Background)
 				</canvas>
 			</div>
 			
-			<div class='w3-col s12 m6 w3-large'>
+			<div class='w3-col s12 m6 w3-large' style='float: right;'>
+				
+				<div class='w3-row w3-small'>
+					&nbsp;
+				</div>
 				
 				<div class='w3-row'>
-					<div class='w3-col s12'>
-						<h2>Peep Defence</h2>
-						<button id='waveBtn' class='w3-btn w3-green'></button>
+					<div class='w3-col s6 w3-xlarge w3-center'>
+						<b>Peep Defence</b>
+					</div>
+					<div class='w3-col s6 w3-xlarge'>
+						<b><button id='waveBtn' class='w3-button w3-purple w3-ripple w3-round-large w3-hover-green'></button></b>
 					</div>
 				</div>
-				<br><br>
+				
+				<div class='w3-row w3-small'>
+					&nbsp;
+				</div>
+				
 				<div class='w3-row'>
 					<div class='w3-col s3'>
 						Health:
 					</div>
 					<div class='w3-col s9'>
-						<div id='gui_health_container' class='w3-red'>
+						<div id='gui_health_container' class='w3-red w3-border w3-card-8'>
 							<div id='gui_health_text'></div>
 							<div id='gui_health' class='w3-container w3-green' style='width:100%'></div>
 						</div>
+					</div>
+				</div>
+				
+				<div class='w3-row w3-padding-top w3-text-orange'>
+					<div class='w3-col s3'>
+						Cash:
+					</div>
+					<div class='w3-col s9'>
+						£ <span id='gui_cash'></span>
 					</div>
 				</div>
 				
@@ -160,10 +182,10 @@ include $partials_folder . '_navbar.php';
 				
 				<div class='w3-row'>
 					<div class='w3-col s3'>
-						Cash:
+						Score:
 					</div>
 					<div class='w3-col s9'>
-						£ <span id='gui_cash'></span>
+						<span id='gui_score'></span>
 					</div>
 				</div>
 				
@@ -175,44 +197,35 @@ include $partials_folder . '_navbar.php';
 						<span id='gui_enemies'></span>
 					</div>
 				</div>
-				
-				<div class='w3-row'>
-					<div class='w3-col s3'>
-						Score:
-					</div>
-					<div class='w3-col s9'>
-						<span id='gui_score'></span>
-					</div>
-				</div>
 			
 			</div>
 			
 		</div>
 		
 		<div id='main-modal' class='w3-modal'>
-		  <div class='w3-modal-content'>
-			    <header class='w3-container w3-purple'> 
+		  <div class='w3-modal-content w3-container w3-card-8 w3-round-large w3-deep-purple'>
+			    <header class='w3-container w3-deep-purple'> 
 				  <span onclick='closeModal()' style='cursor: pointer;'
-					class='w3-button w3-xlarge w3-badge w3-red w3-display-topright'>&times;</span>
+					class='w3-button w3-xlarge w3-badge w3-red w3-display-topright w3-margin'>&times;</span>
 				  <h2 id='main-modal-header'>Modal Header</h2>
 				</header>
 
-				<div class='w3-container w3-text-purple'>
+				<div class='w3-container w3-white w3-text-deep-purple w3-border w3-round-large'>
 					<br>
 					<span id='main-modal-content'></span>
 					<br><br>
 				</div>
 
-				<footer class='w3-container w3-purple w3-center w3-row-padding'>
+				<footer class='w3-container w3-deep-purple w3-center w3-row-padding'>
 					<div class='w3-small'>
 						&nbsp;
 					</div>
 					<div class='w3-col s6 w3-center'>
-						<button id='main-modal-footer-left' class='w3-btn w3-green w3-padding'></button>
+						<b><button id='main-modal-footer-left' class='w3-btn w3-large w3-red w3-ripple w3-round-large w3-padding'></button></b>
 						&nbsp;
 					</div>
 					<div class='w3-col s6 w3-center'>
-						<button id='main-modal-footer-right' class='w3-btn w3-green w3-padding'></button>
+						<b><button id='main-modal-footer-right' class='w3-btn w3-large w3-green w3-ripple w3-round-large w3-padding'></button></b>
 						&nbsp;
 					</div>
 					<div class='w3-small'>
@@ -223,6 +236,7 @@ include $partials_folder . '_navbar.php';
 		</div>
 		
 	</div>
+
 
 <?php
 		
