fdaaa9fb51e595cafc13ca693ecfd91350352a8b
Matt <eclecticappdevelopment@gmail.com>
2018-07-08 21:01:31 +0100

    Tweaks to interface for v2
    Styles moved to separate file, added stuff for advanced features and leaderboards

--- tower-defence.php
+++ tower-defence.php
@@ -7,66 +7,17 @@ $this_type = "website";
 $this_locale = "en_us";
 $this_url = "http://" . $_SERVER["HTTP_HOST"] . $_SERVER['REQUEST_URI'];
 
-$partials_folder = '../templates/partials/';
+$partials_folder = '/home/u545245070/public_html/templates/partials/';
 
-/* STRUCTURE */
+include $partials_folder . '_declare.php';
+include $partials_folder . '_meta.php';
+include $partials_folder . '_social.php';
 
-// _declare.php		(doctype)
-// _meta.php		(meta properties and the site icon)
-// _social.php		(social app_id - FB/Twitter)
-// _styles.php		(stylesheets and fonts)
-// _scripts.php		(scripts which have to be loaded into the head)
-	// echo BODY opening tag
-// _navbar.php		(the nav menu, potentially highlight the current page $this_page / $this_directory)
-	// echo MAIN and CONTENT
-// _footer.php		(footer)
+// NEW - COOKES INFORMATION
+include $partials_folder . '_cookiesHead.php';
 
-/* THESE DON'T WORK ON GIT DEPLOY */
-//include $partials_folder . '_declare.php';
-//include $partials_folder . '_meta.php';
-//include $partials_folder . '_social.php';
-echo "<!DOCTYPE html><html>
-	<!-- VERSION 1.1 on 26-03-2018 -->
-	<head>
-		<link rel='stylesheet' href='/css/w3.css'>
-		<link rel='stylesheet' href='/css/w3-theme-deep-purple.css'>
-		<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Open+Sans'>
-		<script defer src='/css/fontawesome-all.min.js'></script>
-		<style>
-			html,body,h1,h2,h3,h4,h5 {font-family: 'Open Sans', sans-serif}
-		</style>
-
-	
-	";
-	
-echo "<style type='text/css'>
-
-	#gui_health_container {
-		position: relative;
-		height: 25px;
-		z-index: 0;
-	}
-	
-	#gui_health_text {
-		position: relative;
-		width: 100%;
-		height: 25px;
-		z-index: 10;
-		text-align: center;
-	}
-	
-	#gui_health {
-		position: absolute;
-		top: 0px;
-		z-index: 1;
-		height: 25px;
-	}
-	
-	#main-modal {
-		z-index: 1000;
-	}
-	
-</style>";
+// Game specific styles
+echo "<link rel='stylesheet' href='./tower-defence-v2/style/tower-styles.css'/>";
 
 include $partials_folder . '_styles.php';
 include $partials_folder . '_structured.php';
@@ -75,23 +26,31 @@ include $partials_folder . '_structured.php';
 $include_folder_files = [
 
 	// These hold the default values and definitions
-	[ "./tower-defence/defaults", "defaults" ],
-	[ "./tower-defence/defaults", "enemies" ],
-	[ "./tower-defence/defaults", "levels" ],
-	[ "./tower-defence/defaults", "waves" ],
-	[ "./tower-defence/defaults", "modals" ],
+	[ "./tower-defence-v2/defaults", "defaults" ],
+	[ "./tower-defence-v2/defaults", "enemies" ],
+	[ "./tower-defence-v2/defaults", "levels" ],
+	[ "./tower-defence-v2/defaults", "waves" ],
+	[ "./tower-defence-v2/defaults", "towers" ],
+	[ "./tower-defence-v2/defaults", "modals" ],
 
 	// These hold the objects and methods
-	[ "./tower-defence/objects", "bullets" ],
-	[ "./tower-defence/objects", "enemies" ],
-	[ "./tower-defence/objects", "towers" ],
-	[ "./tower-defence/objects", "modals" ],
-	[ "./tower-defence/objects", "canvas" ],
-	[ "./tower-defence/objects", "paths" ],
+	[ "./tower-defence-v2/objects", "bullets" ],
+	[ "./tower-defence-v2/objects", "explosions"],
+	[ "./tower-defence-v2/objects", "enemies" ],
+	[ "./tower-defence-v2/objects", "towers" ],
+	[ "./tower-defence-v2/objects", "game" ],
+	[ "./tower-defence-v2/objects", "gui" ],
+	[ "./tower-defence-v2/objects", "modals" ],
+	[ "./tower-defence-v2/objects", "canvas" ],
+	[ "./tower-defence-v2/objects", "tooltip" ],
+	[ "./tower-defence-v2/objects", "paths" ],
 
 	// This is the main JS file (mainLoop, init, reset)
-	[ "./tower-defence", "tower-defence" ],
-
+	[ "./tower-defence-v2", "main" ],
+	
+	// Leaderboards
+	[ "./tower-defence-v2/leaderboards", "leaderboards" ],
+	
 	// Generic libraries required for this game
 	[ "/libraries/javascript", "window" ],
 	[ "/libraries/javascript", "canvas_events" ],
@@ -105,22 +64,10 @@ for ($i = 0; $i < count($include_folder_files); $i++){
 	$this_file = $include_folder_files[$i][1];
 	echo "<script defer src='" . $this_folder . "/" . $this_file . ".js'></script>";
 }
-	
-/*
-echo "
-	<script src='../libraries/javascript/window.js'></script>
-	<script src='../libraries/javascript/pathfinding-browser.js'></script>
-	<script src='../libraries/javascript/grid.js'></script>
-	<script src='../libraries/javascript/canvas_events.js'></script>
-
-	<script src='./tower-defence/tower-defence.js'></script>
- 	<script src='./tower-defence/tower-defaults.js'></script>
-";
-*/
 
 include $partials_folder . '_scripts.php';	// NOTE, ALSO CLOSES THE HEAD
 
-echo "<body class='w3-dark-grey' onload='init()' onresize='resizeCanvas()'>";
+echo "<body class='w3-dark-grey' onload='EUcookieCheck(); init();' onresize='resizeCanvas()'>";
 
 include $partials_folder . '_navbar.php';
 
@@ -144,20 +91,20 @@ include $partials_folder . '_navbar.php';
 			
 			<div class='w3-col s12 m6 w3-large' style='float: right;'>
 				
-				<div class='w3-row w3-small'>
+				<div class='w3-row w3-tiny'>
 					&nbsp;
 				</div>
 				
 				<div class='w3-row'>
-					<div class='w3-col s6 w3-xlarge w3-center'>
+					<div class='w3-col s6 w3-large w3-center'>
 						<b>Peep Defence</b>
 					</div>
-					<div class='w3-col s6 w3-xlarge'>
-						<b><button id='waveBtn' class='w3-button w3-purple w3-ripple w3-round-large w3-hover-green'></button></b>
+					<div class='w3-col s6 w3-large'>
+						<b><button id='waveBtn' onclick='waveBtnClick()' class='w3-button w3-purple w3-ripple w3-round-large w3-hover-green'></button></b>
 					</div>
 				</div>
 				
-				<div class='w3-row w3-small'>
+				<div class='w3-row w3-tiny'>
 					&nbsp;
 				</div>
 				
@@ -173,45 +120,71 @@ include $partials_folder . '_navbar.php';
 					</div>
 				</div>
 				
-				<div class='w3-row w3-padding-top w3-text-orange'>
+				<div class='w3-row w3-center w3-padding-top w3-text-orange'>
+					<div class='w3-col s2'>
+						Score:
+					</div>
 					<div class='w3-col s3'>
+						<span id='gui_score'></span>
+					</div>
+					<div class='w3-col s2'>
 						Cash:
 					</div>
-					<div class='w3-col s9'>
+					<div class='w3-col s5'>
 						£ <span id='gui_cash'></span>
 					</div>
 				</div>
 				
-				<div class='w3-row'>
+				<div class='w3-row w3-center'>
+					
+					<div class='w3-col s2'>
+						Level:
+					</div>
 					<div class='w3-col s3'>
+						<span id='gui_level'></span>
+					</div>
+				
+					<div class='w3-col s2'>
 						Wave:
 					</div>
-					<div class='w3-col s9'>
-						<span id='gui_wave'></span>
+					<div class='w3-col s5'>
+						(<span id='gui_enemies'></span>)&nbsp;&nbsp;<span id='gui_wave'></span>
 					</div>
+					
 				</div>
 				
-				<div class='w3-row'>
-					<div class='w3-col s3'>
-						Score:
-					</div>
-					<div class='w3-col s9'>
-						<span id='gui_score'></span>
-					</div>
-				</div>
+				<div class='w3-row w3-center w3-large'>
 				
-				<div class='w3-row'>
 					<div class='w3-col s3'>
-						Enemies:
+						<div class='w3-badge w3-circle w3-red w3-padding' onclick='showModal("Settings",0);'>
+							<i class='fas fa-cog'></i>
+						</div>
 					</div>
-					<div class='w3-col s9'>
-						<span id='gui_enemies'></span>
+					<div class='w3-col s3'>
+						<div class='w3-badge w3-circle w3-yellow w3-padding' onclick='showModal("Info",0);'>
+							<i class='fas fa-info'></i>
+						</div>
 					</div>
+					<div class='w3-col s3'>
+						<div class='w3-badge w3-circle w3-orange w3-padding' onclick='showModal("Data",0);'>
+							<i class='fas fa-database'></i>
+						</div>
+					</div>
+					<div class='w3-col s3'>
+						<div class='w3-badge w3-circle w3-green w3-padding' onclick='showModal("Stats & Leaderboards",0);'>
+							<i class='fas fa-trophy'></i>
+						</div>
+					</div>
+					
 				</div>
 			
 			</div>
 			
 		</div>
+		
+		
+		
+		<!-- MODAL WINDOW FOR TUTORIALS AND STORY -->
 		
 		<div id='main-modal' class='w3-modal'>
 		  <div class='w3-modal-content w3-container w3-card-8 w3-round-large w3-deep-purple'>
@@ -248,9 +221,10 @@ include $partials_folder . '_navbar.php';
 		
 	</div>
 
-
 <?php
-		
+
+include $partials_folder . '_cookiesModal.php';
+
 include $partials_folder . '_footer.php';
 
 ?>
