3991a70f3506b6694f614668b5d45a1f103c9fdd
Matt <eclecticappdevelopment@gmail.com>
2018-07-08 21:10:37 +0100

    Removed most defaults
    The default values are moved to JSON file for quicker admin. Now added quick functions to pull and read JSON before game initialisation.

--- tower-defence/defaults/defaults.js
+++ tower-defence/defaults/defaults.js
@@ -8,44 +8,24 @@ var hoverCol = -1;
 var prevCell = 0;
 var enemies = bullets = towers = [];
 
-var options = {
-	segments: 10,
-	rows: 10,
-	columns: 10,
-	health: 10,
-	score: 0,
-	cash: 50,
-	maxHP: 100,
-	default_index: 0,
-	canvas_padding: 5,
-	tower_cost: 25,
-	context_strokeStyle: '#000',
-	context_lineWidth: 1,
-	elapsedFactor: 10,
-	cashFactor: 5,
-	damageFactor:10,
-	enemyHealthFactor: 1,
-	enemySpeedFactor: 2,
-	fireRateFactor: 100
-};
+var defaults = {};
 
-var current = {
-	state: 1,
-	index: 0,	// Picks options.default_index
-	rows: 10,
-	columns: 10,
-	width: 100,
-	height: 100,
-	HP: 100,
-	maxHP: 100,
-	cell_width: 10,
-	cell_height: 10,
-	score: 0,
-	cash: 250,
-	path: [],
-	grid: [],
-	start: [1,1],
-	end: [9,9],
-	tower: false,
-	enemyCount: 0
-};
+function getDefaults(){
+	
+	defaults = newHTTPGet('./tower-defence-v2/defaults/default_values.json');
+	//var defaults = JSON.parse('./default_values.json');
+
+}
+
+function newHTTPGet(yourUrl){
+		
+	var httpRqst = new XMLHttpRequest();
+	httpRqst.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			defaults = JSON.parse(this.responseText);
+			initialiseGame();
+		}
+	};
+	httpRqst.open('GET', yourUrl, true);
+	httpRqst.send();			
+}
