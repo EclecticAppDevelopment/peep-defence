951b88d7b7c8adb6c589055ff734194f3479b643
Matt <eclecticappdevelopment@gmail.com>
2018-07-08 21:12:04 +0100

    Tweaks to some values, renamed enemy
    Changes to values for balance, renamed "Star" enemy to "Air".

--- tower-defence/defaults/enemies.js
+++ tower-defence/defaults/enemies.js
@@ -1,50 +1,55 @@
 const ENEMY_TYPE_BASIC = 0;
 const ENEMY_TYPE_TANK = 1;
 const ENEMY_TYPE_SPEEDER = 2;
-const ENEMY_TYPE_STAR = 3;
+const ENEMY_TYPE_AIR = 3;
 const ENEMY_TYPE_BOSS = 4;
 
 var enemy_type_defaults = [
 	
 	//ENEMY_TYPE_BASIC: {
 	{	
-		radius: 5,
+		radius: 7,
 		speed: 0.1,
-		color: '#f0f',
-		maxHP: 20,
-		shield: 0
+		color: '#fa4',
+		maxHP: 25,
+		shield: 1,
+		name: 'Basic'
 	},
 	//ENEMY_TYPE_TANK: {
 	{
-		radius: 10,
+		radius: 12,
 		speed: 0.05,
-		color: '#f90',
+		color: '#47f',
 		maxHP: 50,
-		shield: 1
+		shield: 10,
+		name: 'Tank'
 	},
 	//ENEMY_TYPE_SPEEDER: {
 	{
-		radius: 5,
+		radius: 12,
 		speed: 0.25,
-		color: '#ff9',
+		color: '#ff4',
 		maxHP: 25,
-		shield: 0
+		shield: 10,
+		name: 'Speeder'
 	},
-	//ENEMY_TYPE_STAR: {
+	//ENEMY_TYPE_AIR: {
 	{
-		radius: 8,
+		radius: 15,
 		speed: 0.1,
-		color: '#f9f',
-		maxHP: 100,
-		shield: 1
+		color: '#4ff',
+		maxHP: 25,
+		shield: 5,
+		name: 'Air'
 	},
 	//ENEMY_TYPE_BOSS: {
 	{
-		radius: 10,
+		radius: 15,
 		speed: 0.05,
-		color: '#f0f',
+		color: '#84f',
 		maxHP: 1000,
-		shield: 5
+		shield: 25,
+		name: 'Boss'
 	}
 	
 ];
