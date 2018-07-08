097caeddcc378bf5fc01d0b7a1f20ae8b72bbc77
Matt <eclecticappdevelopment@gmail.com>
2018-07-08 21:34:02 +0100

    Major enemy changes for version 2
    The new version with colour merging (when damaged, colour turns to red) and some bug fixes

--- tower-defence/objects/enemies.js
+++ tower-defence/objects/enemies.js
@@ -12,7 +12,9 @@ function Enemy(level, type, color, x, y, r, speed, delay, maxHP, shield){
 	this.currentHP = this.maxHP,
 	this.shield = shield || 0,
 	this.path_index = 1,
-	this.flag = false
+	this.flag = false,
+	this.slowed = false,
+	this.slowHits = 0
 }
 
 Enemy.prototype.update = function(timestamp){
@@ -27,8 +29,8 @@ Enemy.prototype.update = function(timestamp){
 	
 	if (this.currentHP <= 0){
 		//console.log('Enemy died!');
-		current.score += this.level;
-		current.cash += this.level * options.cashFactor;
+		game.score += this.level;
+		game.cash += this.level * (this.type + 1) * defaults.cashFactor;
 		return false;
 	}
 	
@@ -39,11 +41,42 @@ Enemy.prototype.update = function(timestamp){
 		
 	}else{
 		
-		// Move along path
-		var targetR = current.path[this.path_index][1];
-		var targetC = current.path[this.path_index][0];
-		var targetX = current.cell_width * targetR + (current.cell_width / 2);
-		var targetY = current.cell_height * targetC + (current.cell_height / 2);
+		// QUICK CHECK - WINDOW MINIMIZE CAUSES ENEMIES TO JUMP OFF SCREEN 
+		if (	
+			(this.x > level.dimensions.width) || (this.x < 0) 
+				||
+			(this.y > level.dimensions.height) || (this.y < 0)
+		){
+			
+			// RESET BACK TO THE START POINT - PLAYERS NOT DISADVANTAGED BY THIS ERROR
+			targetR = level.path[0][0];
+			targetC = level.path[0][1];
+			
+			this.x = level.dimensions.cell_width * targetR + (level.dimensions.cell_width / 2);
+			this.y = level.dimensions.cell_height * targetC + (level.dimensions.cell_height / 2);
+			
+			console.log('AGGRESSIVE CHECK - REVERT ENEMY TO START POINT');
+			
+			// Then carry on processing as normal
+			return true;
+		}
+		
+		if (this.type == ENEMY_TYPE_AIR){
+			
+			// Ignore path
+			var targetR = level.path[level.path.length - 1][1];
+			var targetC = level.path[level.path.length - 1][0];
+			
+		}else{
+		
+			// Move along path
+			var targetR = level.path[this.path_index][1];
+			var targetC = level.path[this.path_index][0];
+			
+		}
+		
+		var targetX = level.dimensions.cell_width * targetR + (level.dimensions.cell_width / 2);
+		var targetY = level.dimensions.cell_height * targetC + (level.dimensions.cell_height / 2);
 		
 		var dist = Math.sqrt( Math.pow(targetX - this.x, 2) + Math.pow(targetY - this.y, 2) );
 
@@ -54,7 +87,7 @@ Enemy.prototype.update = function(timestamp){
 			this.path_index++;
 			
 			// Check if final point
-			if (this.path_index == current.path.length){
+			if (this.path_index == level.path.length){
 				
 				reduceHealth(this.level);
 				return false;
@@ -62,59 +95,152 @@ Enemy.prototype.update = function(timestamp){
 			}else{
 				
 				// Re-target to new point
-				var targetR = current.path[this.path_index][1];
-				var targetC = current.path[this.path_index][0];
-				var targetX = current.cell_width * targetR + (current.cell_width / 2);
-				var targetY = current.cell_height * targetC + (current.cell_height / 2);
+				var targetR = level.path[this.path_index][1];
+				var targetC = level.path[this.path_index][0];
+				var targetX = level.dimensions.cell_width * targetR + (level.dimensions.cell_width / 2);
+				var targetY = level.dimensions.cell_height * targetC + (level.dimensions.cell_height / 2);
 				var dist = Math.sqrt( Math.pow(targetX - this.x, 2) + Math.pow(targetY - this.y, 2) );
 				
 			}
 		}
 		
+		// Handle slowed
+		if (this.slowed >= 0){
+			
+			var slowFact = (this.slowHits + 1);
+			this.slowed -= elapsed;
+			
+		}else{
+			
+			var slowFact = 1;
+			if (this.slowHits >= 1){
+				this.slowHits -= 1;
+			}
+			
+		}
+		
 		// Move towards next path point
-		var xDir = (targetX - this.x) / dist;
-		var yDir = (targetY - this.y) / dist;
-		//elapsed = 1;
-		//this.x += Math.floor( xDir * this.speed * elapsed * options.enemySpeedFactor);
-		this.x += xDir * this.speed * elapsed * options.enemySpeedFactor;
-		//this.y += Math.floor( yDir * this.speed * elapsed * options.enemySpeedFactor);
-		this.y += yDir * this.speed * elapsed * options.enemySpeedFactor;
+		var xDir = ( (targetX - this.x) / dist) / slowFact;
+		var yDir = ( (targetY - this.y) / dist) / slowFact;
+		
+		this.x += Math.min(xDir * this.speed * elapsed * defaults.enemySpeedFactor, level.dimensions.cell_width / defaults.maxMoveDist);//Math.floor( (xDir * this.speed * elapsed * options.enemySpeedFactor) );
+		this.y += Math.min(yDir * this.speed * elapsed * defaults.enemySpeedFactor, level.dimensions.cell_width / defaults.maxMoveDist);//Math.floor( (yDir * this.speed * elapsed * options.enemySpeedFactor) );
+	
 	}
 	return true;
 }
 
 Enemy.prototype.draw = function(ctx){
+	
+	ctx.save();
 	switch (this.type){
-		case 0:
+		case ENEMY_TYPE_BASIC:
 			// Basic - Circle
-			ctx.fillStyle = this.color;
+			//ctx.fillStyle = this.color;
+			
+			var gradient = ctx.createRadialGradient(this.x, this.y, this.r, this.x + (this.r / 4), this.y - (this.r / 4), 2 );
+			gradient.addColorStop(0, this.color);
+			gradient.addColorStop(1, '#fff');
+			ctx.fillStyle = gradient;
+			
 			ctx.beginPath();
 			ctx.arc(this.x, this.y, this.r, 0, 2*Math.PI, false);
 			ctx.fill();
 			ctx.closePath();
 			break;
-		case 1:
+		case ENEMY_TYPE_TANK:
 			// Tough - Square
-			ctx.fillStyle = this.color;
+			//ctx.fillStyle = this.color;
+			var gradient = ctx.createRadialGradient(this.x, this.y, 2 * this.r, this.x + (this.r / 4), this.y - (this.r / 4), 2 );
+			gradient.addColorStop(0, this.color);
+			gradient.addColorStop(1, '#fff');
+			ctx.fillStyle = gradient;
+			
+			
 			ctx.beginPath();
-			ctx.fillRect(this.x - (this.r / 2), this.y - (this.r / 2), this.r, this.r);
+			ctx.fillRect(this.x - this.r, this.y - this.r, 2 * this.r, 2 * this.r);
 			ctx.fill();
 			ctx.closePath();
 			break;
-		case 2:
+		case ENEMY_TYPE_SPEEDER:
 			// Fast - triangle
 			ctx.fillStyle = this.color;
 			ctx.beginPath();
-			ctx.arc(this.x, this.y, this.r, 0, 2*Math.PI, false);
+			ctx.moveTo(this.x-this.r,this.y-this.r);
+			ctx.lineTo(this.x+this.r,this.y-this.r);
+			ctx.lineTo(this.x,this.y+this.r);
+			ctx.lineTo(this.x-this.r,this.y-this.r);
+			ctx.fill();
+			ctx.closePath();
+			break;
+		case ENEMY_TYPE_AIR:
+			// Ignores path - bowtie
+			ctx.fillStyle = this.color;
+			ctx.beginPath();
+			ctx.moveTo(this.x + this.r, this.y - (this.r / 2) );
+			ctx.lineTo(this.x + this.r, this.y + (this.r / 2) );
+			ctx.lineTo(this.x - this.r, this.y - (this.r / 2) );
+			ctx.lineTo(this.x - this.r, this.y + (this.r / 2) );
+			ctx.lineTo(this.x + this.r, this.y - (this.r / 2) );
+			ctx.fill();
+			ctx.closePath();
+			break;
+		case ENEMY_TYPE_BOSS:
+			ctx.fillStyle = this.color;
+			ctx.beginPath();
+			ctx.moveTo(this.x-this.r,this.y-(this.r/2));
+			ctx.lineTo(this.x+this.r,this.y-(this.r/2));
+			ctx.lineTo(this.x-this.r,this.y+this.r);
+			ctx.lineTo(this.x,this.y-this.r);
+			ctx.lineTo(this.x+this.r,this.y+this.r);
+			ctx.lineTo(this.x-this.r,this.y-(this.r/2));
 			ctx.fill();
 			ctx.closePath();
 			break;
 	}
-	// Draw health bar
-	//drawHealthBar(ctx, this.x, this.y, this.r, this.currentHP, this.maxHP);
+	
+	if (this.shield > 0){
+		// Draw shield
+		var shieldR = this.r * 1.5;
+		var shieldCol = '#0ff';
+		var thick_fact = 10;
+		var thickness = Math.max(1,Math.floor(this.shield / thick_fact) ) + 'px';
+		
+		ctx.strokeStyle = shieldCol;
+		ctx.lineWidth = thickness;
+		ctx.beginPath();
+		ctx.arc(this.x, this.y, shieldR, 0, 2*Math.PI, false);
+		ctx.stroke();
+		ctx.closePath();
+	}
+	ctx.restore();
 	
 }
 
+// Takes a current colour and returns a new, merged colour
+// Merge depends on the base and final colours, and the percentage change
+// The current colour is not changed, instead a new colour is returned
+function color_merge(base, end, percent, current){
+	
+	var base_dec = parseInt( base.replace("#",""), 16);
+	var end_dec = parseInt( end.replace("#",""), 16);
+	var current_dec = parseInt( current.replace("#",""), 16);
+		//console.log('Col Mrg: ',base_dec,end_dec,current_dec);
+	
+	var col_diff = base_dec - end_dec;
+	var col_sub = Math.floor(col_diff * percent);
+	
+	var new_dec = current_dec - col_sub;
+		//console.log(col_diff,col_sub,new_dec);
+
+	var new_col = "#" + new_dec.toString(16);
+		//console.log('Col Merge: ',percent,'%',current,'->',new_col);
+	
+	return new_col;
+	}
+
+/*
+// Now changing enemy draw color from default -> red
 function drawHealthBar(ctx, x, y, r, val, max){
 	console.log('drawHealthBar',x, y, r, val, max);
 	var percent = (val / max);
@@ -129,10 +255,15 @@ function drawHealthBar(ctx, x, y, r, val, max){
 	ctx.fill();
 	ctx.closePath();
 	ctx.restore();
-}
+}*/
 
+// Generates a single enemy based on defaults type values
 function generateOneEnemy(type,level){
+	
+	// Enemy Object defined at top
 	var enemy = new Enemy();
+	
+	// Type and level are the only paramaters needed
 	enemy.type = type;
 	enemy.level = level;
 	
@@ -140,16 +271,21 @@ function generateOneEnemy(type,level){
 	enemy.r = enemy_type_defaults[type].radius;
 	enemy.color = enemy_type_defaults[type].color;
 	
-	enemy.speed = level * enemy_type_defaults[type].speed;	
-	enemy.maxHP = options.enemyHealthFactor * level * enemy_type_defaults[type].maxHP;
+	// Speed NOT linked to level, as this causes distance calculations difficult
+	enemy.speed = enemy_type_defaults[type].speed; //* level;	
+	
+	// However, HP and Shields (not currently implemented) ARE multiplied by level
+	enemy.maxHP = defaults.enemyHealthFactor * level * enemy_type_defaults[type].maxHP;
 	enemy.currentHP = enemy.maxHP;
-	enemy.shield = level * enemy_type_defaults[type].shield;
+	//enemy.shield = level * enemy_type_defaults[type].shield;
 
 	return enemy;
 }
 
+// Generates a wave of enemies based on the default/waves
 function generateWave(wave,spawn_x,spawn_y){
 	
+	// Get the overall wave info
 	waveInfo = waveMaps[wave];
 	waveDelay = waveInfo.wave_delay;	//Added to all enemies in subwave 1-n
 	waveCount = waveInfo.enemies.length;
@@ -162,12 +298,14 @@ function generateWave(wave,spawn_x,spawn_y){
 		
 		subwaveType = subwaveInfo.type;
 		subwaveLevel = subwaveInfo.level;
+		subwaveShield = subwaveInfo.shield;
 		
 		for (var j = 0; j < subwaveCount; j++){
 			
 			var newEnemy = generateOneEnemy(subwaveType,subwaveLevel);
 			newEnemy.x = spawn_x;
-			newEnemy.y = spawn_y;			
+			newEnemy.y = spawn_y;	
+			newEnemy.shield = defaults.enemyShieldFactor * subwaveShield * enemy_type_defaults[subwaveType].shield;
 			newEnemy.delay = (j * subwaveDelay) + (i * waveDelay);
 			
 			enemies.push(newEnemy);
@@ -176,47 +314,9 @@ function generateWave(wave,spawn_x,spawn_y){
 		
 	}
 	//console.log(enemies);
-	current.enemyCount = enemies.length;
+	level.enemyCount = enemies.length;
 	window.requestAnimationFrame(mainLoop);
 
 }
 
-function OLDgenerateEnemies(wave,spawn_x,spawn_y){
-	
-	waveInfo = waveMaps[wave];
-	//console.log(wave,waveInfo);
-	waveEnemies = waveInfo.enemies;
-	waveDelay = waveInfo.wave_delay;	//Added to all enemies in subwave 1-n
-	waveCount = waveInfo.enemies.length;
-	
-	for (var i = 0; i < waveCount; i++){
-		
-		subwaveInfo = waveInfo.enemies[i];
-		subwaveCount = subwaveInfo.count;
-		subwaveDelay = subwaveInfo.subwave_delay;
-		
-		for (var j = 0; j < subwaveCount; j++){
-			
-			// Trick is each enemy's delay
-			// part one is j * subwave_delay - from 0 to maxDelay
-			// and also i * wave_delay - from 0 to largerDelay
-			var thisEnemyDelay = (j * subwaveDelay) + (i * waveDelay);
-			enemies.push( new Enemy(
-				subwaveInfo.level, 
-				subwaveInfo.type, 
-				subwaveInfo.color,
-				spawn_x, 
-				spawn_y, 
-				subwaveInfo.radius, 
-				subwaveInfo.speed, 
-				thisEnemyDelay,
-				subwaveInfo.maxHP, 
-				subwaveInfo.shield
-			));
-		}
-		
-	}
-	
-	window.requestAnimationFrame(mainLoop);
 
-}
