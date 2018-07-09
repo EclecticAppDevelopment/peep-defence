c1fa9fb6ad5989b2f58d677ae73b3bdb9dfdb229
Matt <eclecticappdevelopment@gmail.com>
2018-07-09 10:38:48 +0100

    New file - sets up connection to leaderboard DB

---
+++ tower-defence/leaderboards/leaderboard-setup.php
@@ -0,0 +1,12 @@
+<?php
+
+  /* Setup DB variables */
+  $sql_host = "mysql.hostinger.co.uk";
+	$sql_db = "XXXXX"; // Anonymised
+	$sql_user = "XXXXX"; // Anonymised
+  $sql_pass = "XXXXX";  // Anonymised
+
+  /* Create the sql connection */
+  $sql_conn = new mysqli($sql_host, $sql_user, $sql_pass, $sql_db);
+
+?>
